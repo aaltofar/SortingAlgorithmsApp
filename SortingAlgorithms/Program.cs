26ee40d1ab1b48cd31b4d4243ab3f46341b662d5
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2023-01-25 15:12:06 +0100

    Add project files.

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Random r = new Random();
+int[] arrToSort = new int[50];
+
+for (int i = 0; i < 50; i++)
+{
+    arrToSort[i] = r.Next(2, 50);
+}
+
---
+++ SortingAlgorithms/SortingMethods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms;
+
+internal class SortingMethods
+{
+    public int[] BubbleSort(int[] arr)
+    {
+        var num = arr.Length;
+        for (int i = 0; i < num - 1; i++)
+        {
+            for (int j = 0; j < num - i - 1; j++)
+            {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        return arr;
+    }
+
+    public int[] InsertionSort(int[] arr)
+    {
+
+        return arr;
+    }
+
+    public void PrintArr(int[] ints)
+    {
+
+        foreach (var n in ints)
+        {
+            Console.Write(n);
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write("#");
+                Console.ResetColor();
+            }
+        }
+    }
+}
+
